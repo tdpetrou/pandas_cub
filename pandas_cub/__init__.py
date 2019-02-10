e4e6210ed0bebfa6a90fbf4a42873438a3903433
tdpetrou <petrou.theodore@gmail.com>
2019-02-09 21:25:47 -0500

    str.lower

--- pandas_cub/__init__.py
+++ pandas_cub/__init__.py
@@ -730,6 +730,9 @@ class StringMethods:
     def title(self, col):
         return self._str_method(str.title, col)
 
+    def lower(self, col):
+        return self._str_method(str.lower, col)
+
     def upper(self, col):
         return self._str_method(str.upper, col)
 
